755b48b528c1932407468ceb6b10ca436ad98d1b
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2024-07-06 07:50:21 +0700

    Mempelajari Operator Penugasan

---
+++ OperatorPenugasan.php
@@ -0,0 +1,15 @@
+<?php 
+
+$number = 10;
+$number += 5; // $number = $number + 5
+var_dump($number);
+$number -= 5; // $number = $number - 5
+var_dump($number);
+$number *= 5; // $number = $number * 5
+var_dump($number);
+$number /= 5; // $number = $number / 5
+var_dump($number);
+$number %= 5; // $number = $number % 5 
+var_dump($number);
+$number **= 5; // $number = $number ** 5
+var_dump($number);
