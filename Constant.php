209b32522f6421ce66ec35f954242fceac851ac2
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2024-07-06 07:20:30 +0700

    Mempelajari Constant

---
+++ Constant.php
@@ -0,0 +1,16 @@
+<?php 
+
+// immutable variables
+// cannot redeclare constant
+// best practice using UPPERCASE
+
+define("PI", 3.14);
+define("AUTHOR", "John Doe");
+define("AUTHOR_ADDRESS", "123 Main St, Anytown, USA");
+
+echo PI;
+echo "\n";
+echo AUTHOR;
+echo "\n";
+echo AUTHOR_ADDRESS;
+echo "\n";
