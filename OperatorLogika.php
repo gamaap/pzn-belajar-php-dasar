0bbdb9fc0cdd0ca62f54ee67ff3e100015e0e45c
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2024-07-06 08:02:20 +0700

    Mempelajari Operator Logika

---
+++ OperatorLogika.php
@@ -0,0 +1,10 @@
+<?php 
+
+var_dump(true && true); // true
+var_dump(true && false); // false
+var_dump(true || true); // true
+var_dump(true || false); // true
+var_dump(!true); // false
+var_dump(!false); // true
+var_dump(true xor true); // false
+var_dump(true xor false); // true
